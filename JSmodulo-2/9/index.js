160d82e9dedfe4af14cd81ad8f714757b8e39e1f
llucas0109 <lpcampestrini@gmail.com>
2023-09-16 13:18:38 -0300

    input de nacionalidade usando java

---
+++ JSmodulo-2/9/index.js
@@ -0,0 +1,16 @@
+var vel = 10.5 //Lembrando que valores com pontos sao chamados de reais
+//Colocamos com craze para poder interpolar
+console.log(`a velocidade do seucaroo é ${vel}km/h`)
+if(vel>120){ //Condiçao simples.
+    console.log(`Voce ultrapassou a velocidade permitida.MULTADO!`)
+}
+else{
+    console.log('Dirija sempre usando sinto de segurança!')
+}
+var pais ='EUA'
+console.log(`Vivendo ${pais}`)
+if(pais == 'BRASIL'){
+    console.log(`voce é BRASILEIRO`)
+}else{
+    console.log(`voce é Estrangeiro`)
+}
